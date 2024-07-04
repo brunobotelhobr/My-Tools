f3389a204fb40990f74e05fba753324511cf607b
Bruno Botelho <bruno.botelho.br@gmail.com>
2024-07-04 14:11:47 -0300

    Refactor ICMP monitoring scripts for improved readability and maintainability

--- http/http-get-server.py
+++ http/http-get-server.py
@@ -1,57 +1,122 @@
 #!/usr/bin/python
 ############################################################
+# Requirements:
+# pip install termcolor
+############################################################
 
-import urllib2
-from scapy.all import *
+from http.server import BaseHTTPRequestHandler, HTTPServer
 from termcolor import colored
-import base64
+import requests
 import datetime
 import argparse
 import os
-from http.server import BaseHTTPRequestHandler, HTTPServer
-from urlparse import urlparse, parse_qs
+import time
+import logging
+import base64
 
-# Arguments
-parser = argparse.ArgumentParser()
-parser.add_argument('-f','--write-file', action='store', required=True, dest='read_file',help='[Required] File to store information')
-parser.add_argument('-ip','--server-ip', action='store', required=True, dest='server_ip',help='[Required] Server IP Address')
-parser.add_argument('-p','--target-port', action='store', required=True, dest='port',help='[Required] Server Port.')
-args = parser.parse_args()
+# Disable scapy warning output
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 
-# Configuration
-dump_file = str(args.read_file)
-http_server_port = int(args.port)
-server_ip = str(args.server_ip)
+NAME = "HTTP GET Server"
+VERSION = "1.0"
+DATE = "02/06/2024"
 
-# Header
-print ('###[ HTTP Server GET Writer ]###')
-print ('###[ Version 0.2 ]###')
-print ('###[ Date 22/04/2020 ]###')
-print ('###[ by Bruno Botelho - bruno.botelho.br@gmail.com ]###')
-print ('')
 
-print ('###[ Starting HTTP Server Monitoring ]###')
-print ('###[ File to Write: ' + dump_file + ' ]###')
-print ('')
+def parse_arguments():
+    """Parse and return arguments from the command line."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-w",
+        "--write-file",
+        action="store",
+        dest="write_file",
+        required=True,
+        help="File to write the HTTP Get.",
+    )
+    parser.add_argument(
+        "-l",
+        "--listener_address",
+        action="store",
+        dest="listener_address",
+        required=True,
+        help="Listener Address.",
+    )
+    parser.add_argument(
+        "-p",
+        "--port",
+        action="store",
+        dest="port",
+        required=True,
+        help="Listener Port.",
+    )
+    return parser.parse_args()
+
+
+def print_banner():
+    """Print the banner."""
+    print("")
+    print(f"### {NAME}")
+    print(f"### Version {VERSION}")
+    print(f"### Date {DATE}")
+    print("### by Bruno Botelho - bruno.botelho.br@gmail.com")
+    print("")
+
 
 def write(j):
-    file = open(dump_file, 'a')
+    """Write the HTTP Get to a file."""
+    file = open(parse_arguments().write_file, "a", encoding="utf-8")
     file.write(j)
     file.close()
 
+
+def log_timestamp():
+    """Return the current timestamp."""
+    return datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+
+
 class StoreHandler(BaseHTTPRequestHandler):
+    """Store Handler Class."""
+
     def do_GET(self):
-        parameters = parse_qs(urlparse(self.path).query)
-        index_c = parameters['p']
-        p = str(index_c).decode('base64').split('&&')
+        """Handle the GET request."""
+        parameters = self.path.split("?")
+        index_c = parameters[1]
+        p = str(base64.b64decode(index_c))
+        p = p.split("&&")
         index = p[1]
-        index_c = p[0] + '&&' + index
-        raw=index_c.encode('base64')
-        print ('###[ Received: ' + colored(index,'green') + ' Get > ' + 'http://' + server_ip + '/sys?p=' + colored(str(raw).rstrip('\n'),'red') + ' ]###')
+        index_c = p[0] + "&&" + index
+        print(
+            log_timestamp()
+            + " Received: "
+            + colored(index, "green")
+            + " Get > "
+            + "http://"
+            + self.client_address[0]
+            + "/sys?p="
+            + colored(str(index_c).rstrip("\n"), "red")
+            + " ]###"
+        )
         write(p[0])
         self.send_response(200)
-    def log_message(self, format, *args):
-        return 
 
-server = HTTPServer(('', 80), StoreHandler)
-server.serve_forever()
+    def log_message(self, format, *args):
+        return
+
+
+def main():
+    """Main function."""
+    args = parse_arguments()
+    print_banner()
+
+    print("### Starting HTTP Server Monitoring")
+    print("### Listener Address: " + args.listener_address)
+    print("### Listener Port: " + args.port)
+    print("### File to Write: " + args.write_file)
+    print("")
+
+    server = HTTPServer((args.listener_address, int(args.port)), StoreHandler)
+    server.serve_forever()
+
+
+if __name__ == "__main__":
+    main()
--- http/http-get-transfer.py
+++ http/http-get-transfer.py
@@ -1,72 +1,128 @@
 #!/usr/bin/python
 ############################################################
+# Requirements:
+# pip install termcolor
+############################################################
 
-import urllib2
-from scapy.all import *
 from termcolor import colored
-import base64
+import requests
 import datetime
 import argparse
 import os
+import time
+import logging
+import base64
 
-# Arguments
-parser = argparse.ArgumentParser()
-parser.add_argument('-f','--read-file', action='store', required=True, dest='read_file',help='[Required] File to send by HTTP Get.')
-parser.add_argument('-t','--target-address', action='store', required=True, dest='target',help='[Required] Destination Address .')
-parser.add_argument('-p','--target-port', action='store', required=True, dest='port',help='[Required] Destination Port.')
-args = parser.parse_args()
+# Disable scapy warning output
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 
-# Configuration
-file_to_tranfer = str(args.read_file)
-http_server = str(args.target)
-http_server_port = int(args.port)
+NAME = "HTTP GET Transfer"
+VERSION = "1.0"
+DATE = "02/06/2024"
 
-# Header
-print ('###[ HTTP GET Transfer ]###')
-print ('###[ Version 0.2 ]###')
-print ('###[ Date 22/04/2020 ]###')
-print ('###[ by Bruno Botelho - bruno.botelho.br@gmail.com ]###')
-print ('')
+
+def parse_arguments():
+    """Parse and return arguments from the command line."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-f",
+        "--read-file",
+        action="store",
+        dest="read_file",
+        required=True,
+        help="File to send by HTTP Get.",
+    )
+    parser.add_argument(
+        "-t",
+        "--target-address",
+        action="store",
+        dest="target",
+        required=True,
+        help="Destination Address.",
+    )
+    parser.add_argument(
+        "-p",
+        "--target-port",
+        action="store",
+        dest="port",
+        required=True,
+        help="Destination Port.",
+    )
+    return parser.parse_args()
+
+
+def print_banner():
+    """Print the banner."""
+    print("")
+    print(f"### {NAME}")
+    print(f"### Version {VERSION}")
+    print(f"### Date {DATE}")
+    print("### by Bruno Botelho - bruno.botelho.br@gmail.com")
+    print("")
+
 
 def log_timestamp():
+    """Return the current timestamp."""
     return datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 
+
 def convert_bytes(num):
-    for x in ['bytes', 'KB', 'MB', 'GB', 'TB']:
+    """Convert bytes to KB, MB, GB, TB as needed."""
+    for x in ["bytes", "KB", "MB", "GB", "TB"]:
         if num < 1024.0:
             return "%3.1f %s" % (num, x)
         num /= 1024.0
 
+
 def file_size(file):
+    """Return the file size."""
     if os.path.isfile(file):
         file_info = os.stat(file)
         return convert_bytes(file_info.st_size)
 
+
 def do_get(a):
-    try:
-        return urllib2.urlopen(a)
-    except:
-        time.sleep(1)
-        do_get(a)
+    """Do the HTTP Get request."""
+    try:  # type: ignore
+        return requests.get(a, timeout=5)  # type: ignore
+    except:  # type: ignore
+        time.sleep(1)  # type: ignore
+        do_get(a)  # type: ignore
 
-file = open(file_to_tranfer, "r", buffering=-1)
-file_data = file.read()
-file.close()
 
-print ('###[ '+ log_timestamp() + ' File to Transfer: ' + file_to_tranfer + ' ]###')
-print ('###[ '+ log_timestamp() + ' File Size: ' + file_size(file_to_tranfer) + ' ]###')
-print ('###[ '+ log_timestamp() + ' File Characters: ' + str(len(str(file_data))) + ' ]###')
-print ('')
+def main():
+    """Main function."""
+    args = parse_arguments()
+    print_banner()
 
-file_characters = len(str(file_data))
+    file = open(args.read_file, "r")
+    file_data = file.read()
+    file.close()
 
-i = 0
+    print("### " + log_timestamp() + " File to Transfer: " + str(args.read_file))
+    print("### " + log_timestamp() + " File Size: " + str(file_size(args.read_file)))
+    print("### " + log_timestamp() + " Destination Address: " + args.target)
+    print("### " + log_timestamp() + " Destination Port: " + args.port)
+    print("")
 
-for c in file_data:
-    i = i + 1
-    index = str(i) + '/' + str(len(file_data))
-    index_c = c + '&&' + index
-    raw=index_c.encode('base64')
-    url_get = 'http://' + http_server + ':' + str(http_server_port) + '/sys?p=' + str(raw)
-    print ('###[ '+ log_timestamp() + ' Transfering: ' + colored(index,'green') + ' Get > ' + colored(str(url_get).rstrip('\n'),'red') + ' ]###')
-    do_get(url_get)
+    i = 0
+
+    for c in file_data:
+        i = i + 1
+        index = str(i) + "/" + str(len(file_data))
+        index_c = c + "&&" + index
+        index_c_bytes = index_c.encode("utf-8")  # Convert to bytes
+        payload = base64.b64encode(index_c_bytes).decode("utf-8")
+        url_get = "http://" + args.target + ":" + str(args.port) + "/sys?p=" + payload
+        print(
+            log_timestamp()
+            + " Transfering: "
+            + colored(index, "green")
+            + " Get > "
+            + colored(str(url_get).rstrip("\n"), "red")
+        )
+        do_get(url_get)
+
+
+if __name__ == "__main__":
+    main()
